68e5563845fdf2459ea75eb5580d28fce2f73e00
Lenovo <3153566913@qq.com>
2023-04-05 15:46:26 +0800

    up

--- src/main/java/com/mybatisplus/com/Application.java
+++ src/main/java/com/mybatisplus/com/Application.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
+        log.info("Application started");
     }
-
 }
